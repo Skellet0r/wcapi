759ec79a7adfe1cfd487ba47c7251eacf1a62a49
skelletor <edward.amor3@gmail.com>
2019-09-24 15:36:39 -0400

    Screen class property declarations

---
+++ wccom/screen.py
@@ -0,0 +1,39 @@
+class Screen:
+    def __init__(self, _object):
+        self._screen = _object
+
+    @property
+    def Application(self):
+        pass
+
+    @property
+    def Col(self):
+        pass
+
+    @property
+    def InHiddenField(self):
+        pass
+
+    @property
+    def Name(self):
+        pass
+
+    @property
+    def OIA(self):
+        pass
+
+    @property
+    def Parent(self):
+        pass
+
+    @property
+    def Row(self):
+        pass
+
+    @property
+    def Rows(self):
+        pass
+
+    @property
+    def Selection(self):
+        pass
