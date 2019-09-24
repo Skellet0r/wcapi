9e53660cbc2bb4163ac16b8db75127f70b9c4aaf
skelletor <edward.amor3@gmail.com>
2019-09-24 15:29:57 -0400

    Area object updated
    Added definitions to all properties

--- wccom/area.py
+++ wccom/area.py
@@ -1,46 +1,56 @@
 class Area:
-
     def __init__(self, _object):
         self._area = _object
-    
+
     @property
     def Application(self):
-        pass
+        """Return System object. Read-only."""
+        return self._area.Application
 
     @property
     def Bottom(self):
-        pass
+        """Returns or sets the ending row of the Area object. ReadWrite."""
+        return self._area.Bottom
 
     @property
     def Left(self):
-        pass
+        """Returns or sets the screen column where the area begins. ReadWrite."""
+        return self._area.Left
 
     @property
     def Parent(self):
-        pass
+        """Returns the parent (Screen) object for the Area object. Read-only."""
+        return self._area.Parent
 
     @property
     def Right(self):
-        pass
+        """Returns or sets the screen column where the area ends. ReadWrite."""
+        return self._area.Right
 
     @property
     def Top(self):
-        pass
+        """Returns or sets the beginning row of the Area object. ReadWrite."""
+        return self._area.Top
 
     @property
     def Type(self):
-        pass
+        """States how to interpreted the top, left, bottom and right coordinates. ReadWrite.
+        WCOI only supports xSTREAM and xNONE options.
+        Integer xSTREAM = 2
+        Integer xNONE = 0"""
+        return self._area.Type
 
     @property
     def Value(self):
-        pass
+        """Returns or sets the text in the Area. ReadWrite."""
+        return self._area.Value
 
     @property
     def Copy(self):
-        pass
+        """Copies the select text to the Clipboard."""
+        return self._area.Copy
 
     @property
     def Select(self):
-        pass
-
-    
+        """Selects the Object."""
+        return self._area.Select
