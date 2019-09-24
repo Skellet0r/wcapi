c8c16843bfa62415107013f0b99efdd75aa630b0
skelletor <edward.amor3@gmail.com>
2019-09-24 15:23:50 -0400

    Area class skeleton, declaration of properties

---
+++ wccom/area.py
@@ -0,0 +1,46 @@
+class Area:
+
+    def __init__(self, _object):
+        self._area = _object
+    
+    @property
+    def Application(self):
+        pass
+
+    @property
+    def Bottom(self):
+        pass
+
+    @property
+    def Left(self):
+        pass
+
+    @property
+    def Parent(self):
+        pass
+
+    @property
+    def Right(self):
+        pass
+
+    @property
+    def Top(self):
+        pass
+
+    @property
+    def Type(self):
+        pass
+
+    @property
+    def Value(self):
+        pass
+
+    @property
+    def Copy(self):
+        pass
+
+    @property
+    def Select(self):
+        pass
+
+    
