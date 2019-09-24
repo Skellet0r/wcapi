00887cd5ba3bd49ebdcf3d8ac451e8481182c248
skelletor <edward.amor3@gmail.com>
2019-09-24 15:40:23 -0400

    Screen object property definitions added

--- wccom/screen.py
+++ wccom/screen.py
@@ -4,36 +4,49 @@ class Screen:
 
     @property
     def Application(self):
-        pass
+        """Returns the System Object Read-only."""
+        return self._screen.Application
 
     @property
     def Col(self):
-        pass
+        """Returns or sets the column position of the cursor. First column is 1."""
+        return self._screen.Col
 
     @property
     def InHiddenField(self):
-        pass
+        """Returns TRUE if the cursor is within a hidden (password) field,
+        otherwise FALSE. This property is valid only for 3270 or 5250 emulation
+        types. Read-only."""
+        return self._screen.InHiddenField
 
     @property
     def Name(self):
-        pass
+        """Returns name of the object as a string Read-only."""
+        return self._screen.Name
 
     @property
     def OIA(self):
-        pass
+        """Returns an OIA object."""
+        return self._screen.OIA
 
     @property
     def Parent(self):
-        pass
+        """Returns parent for the specified object. (Session). Read-only"""
+        return self._screen.Parent
 
     @property
     def Row(self):
-        pass
+        """Returns or sets the row position of the cursor. First row is 1."""
+        return self._screen.Row
 
     @property
     def Rows(self):
-        pass
+        """Returns the number of rows in the presentation space. Read-only.
+        First row is 1."""
+        return self._screen.Rows
 
     @property
     def Selection(self):
-        pass
+        """Returns an Area object representing the area of the screen currently
+        selected by the user. Read-only."""
+        return self._screen.Selection
