f4f5096bab866b1e46c85e5722626158133fbccc
skelletor <edward.amor3@gmail.com>
2019-09-25 10:05:41 -0400

    system object property declarations

---
+++ wccom/system.py
@@ -0,0 +1,39 @@
+import comtypes.client as cc
+
+
+class System:
+    def __init__(self, _object=None):
+        self._system = _object if _object else cc.CreateObject("WebConnect.System")
+
+    @property
+    def ActiveSession(self):
+        pass
+
+    @property
+    def Application(self):
+        pass
+
+    @property
+    def FullName(self):
+        pass
+
+    @property
+    def Name(self):
+        pass
+
+    @property
+    def Parent(self):
+        pass
+
+    @property
+    def Sessions(self):
+        pass
+
+    @property
+    def TimeoutValue(self):
+        pass
+
+    @property
+    def Version(self):
+        pass
+
