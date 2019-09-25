41f1b98781ae49219528cdc6a8a6268c6dc5fc90
skelletor <edward.amor3@gmail.com>
2019-09-25 08:43:54 -0400

    Session object property declarations

---
+++ wccom/session.py
@@ -0,0 +1,31 @@
+class Session:
+    def __init__(self, _object):
+        self._session = _object
+
+    @property
+    def Application(self):
+        pass
+
+    @property
+    def Connected(self):
+        pass
+
+    @property
+    def FileTransferHostOS(self):
+        pass
+
+    @property
+    def FullName(self):
+        pass
+
+    @property
+    def Name(self):
+        pass
+
+    @property
+    def Screen(self):
+        pass
+
+    @property
+    def Type(self):
+        pass
