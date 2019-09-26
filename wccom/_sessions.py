bb8bbf27de6667b21aa51db4f0564cd3576a1152
skelletor <edward.amor3@gmail.com>
2019-09-26 18:20:31 -0400

    sessions object property declarations

---
+++ wccom/_sessions.py
@@ -0,0 +1,52 @@
+class Sessions:
+    def __init__(self, _object):
+        self._sessions = _object
+
+
+    @property
+    def Application(self):
+        pass
+
+    @property
+    def Count(self):
+        pass
+
+    @property
+    def Parent(self):
+        pass
+
+    @property
+    def WCCertificate(self):
+        pass
+
+    @property
+    def WCCipherSuite(self):
+        pass
+
+    @property
+    def WCHTMLByPass(self):
+        pass
+
+    @property
+    def WCServerName(self):
+        pass
+
+    @property
+    def WCSessionType(self):
+        pass
+
+    @property
+    def WCServerPort(self):
+        pass
+
+    @property
+    def WCServerUserid(self):
+        pass
+
+    @property
+    def WCServerPasswd(self):
+        pass
+
+    @property
+    def WCProxy(self):
+        pass
